715f6b5219c7881e2618389db85750769d55ec72
Your Name <you@example.com>
2024-10-11 20:34:19 -0300

    Algo A mais

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:trabalho_2/Pessoa.dart';
+import 'package:trabalho_2/PessoaDAO.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,26 +20,135 @@ class MyApp extends StatelessWidget {
 
 class Pagina1 extends StatefulWidget {
   const Pagina1({super.key});
-
   @override
   State<Pagina1> createState() => _Pagina1State();
 }
 
 class _Pagina1State extends State<Pagina1> {
+  final Pessoadao _pessoaDAO = Pessoadao();
+
+  final TextEditingController _nomeController = TextEditingController();
+  final TextEditingController _cpfcontroller = TextEditingController();
+  final TextEditingController _cartaoController = TextEditingController();
+
+  Pessoa? _pessoaAtual;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadPessoas();
+  }
+
+  List<Pessoa> _listaPessoas = [];
+
+  void _loadPessoas() async {
+    List<Pessoa> listaTemp = await _pessoaDAO.selectPessoa();
+    setState(() {
+      _listaPessoas = listaTemp;
+    });
+  }
+
+  void _editarPessoa(Pessoa pessoa) {
+    setState(
+      () {
+        _pessoaAtual = pessoa;
+        _nomeController.text = pessoa.nome;
+        _cpfcontroller.text = pessoa.cpf;
+        _cartaoController.text = pessoa.credcard;
+      },
+    );
+  }
+
+  Future<void> _deletePessoa(int index) async {
+    await _pessoaDAO.deletePessoa(
+      Pessoa(
+        id: index,
+        nome: '',
+        cpf: '',
+        credcard: '',
+      ),
+    );
+    _loadPessoas();
+  }
+
+  Future<void> _salvarOuEditar() async {
+    if (_pessoaAtual == null) {
+      await _pessoaDAO.insertPessoa(
+        Pessoa(
+            nome: _nomeController.text,
+            cpf: _cpfcontroller.text,
+            credcard: _cartaoController.text),
+      );
+    } else {
+      _pessoaAtual?.nome = _nomeController.text;
+      _pessoaAtual?.cpf = _cpfcontroller.text;
+      _pessoaAtual?.credcard = _cartaoController.text;
+      await _pessoaDAO.updatePessoa(_pessoaAtual!);
+    }
+    _nomeController.clear();
+    _cpfcontroller.clear();
+    _cartaoController.clear();
+    setState(
+      () {
+        _pessoaAtual = null;
+      },
+    );
+    _loadPessoas();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('AppBar?'),
+        title: const Text('AppBar?'),
       ),
-      body: const Column(
+      body: Column(
         children: [
-          Padding(padding: EdgeInsets.all(10.0),
-          child: TextField(
-            decoration: InputDecoration(
-              labelText: 'Nome'
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              controller: _nomeController,
+              decoration: const InputDecoration(labelText: 'Nome'),
             ),
-          ),),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              controller: _cpfcontroller,
+              decoration: const InputDecoration(labelText: 'CPF'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              controller: _cartaoController,
+              decoration: const InputDecoration(labelText: 'Nº Cartao'),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: _salvarOuEditar,
+            child: Text(_pessoaAtual == null ? 'Salvar' : 'Atualizar'),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _listaPessoas.length,
+              itemBuilder: (context, index) {
+                final Pessoa pessoa = _listaPessoas[index];
+                return ListTile(
+                  title: Text('Nome: ${pessoa.nome} - CPF: ${pessoa.cpf}'),
+                  trailing: IconButton(
+                    onPressed: () {
+                      _deletePessoa(pessoa.id!);
+                    },
+                    icon: const Icon(Icons.delete),
+                  ),
+                  onTap: () {
+                    _editarPessoa(pessoa);
+                  },
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
